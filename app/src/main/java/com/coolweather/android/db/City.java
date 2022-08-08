f184937622b068fdf0bc913dc195bb949dd3214a
cohayashirihensan <1466370506@qq.com>
2022-08-09 00:34:48 +0800

    加入創建數據庫和表的各項配置

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,44 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProviceId() {
+        return proviceId;
+    }
+
+    public void setProviceId(int proviceId) {
+        this.proviceId = proviceId;
+    }
+
+    private int proviceId;
+
+}
